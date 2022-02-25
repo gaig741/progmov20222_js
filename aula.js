9d54b4dd6d0810a13df29c171159a5edc885d763
caique <caiquetrivino@hotmail.com>
2022-02-25 18:05:41 -0300

    teste vetores em js

--- aula.js
+++ aula.js
@@ -1,1 +1,42 @@
-console.log (false == 0)
+const nomes = ["Ana Maria", "Antonio", "Felipe", "Ananias"]
+
+// Arrow function "(n) =>"
+// Verificando se o nome começa com A
+const resultante = nomes.filter((n) => {
+    return n.startsWith("A")
+}) 
+console.log(resultante)
+
+/* função em C = void f (int addEventListener, float b){
+    
+} */
+
+
+/* class Pessoa{
+    public void andar (){
+
+    }
+
+    variavel = andar;
+} */
+
+
+/* const nomes = [1, 2]
+console.log (nomes)
+nomes[0] = 3
+console.log(nomes)
+nomes[255] = "oi"
+console.log(nomes.length) */
+
+
+/* v1 = []
+v1[0] = 3.4
+v1[10] = 2
+v1[2] = "abc"
+
+console.log(v1.length)
+console.log (v1)
+
+for (let i=0; i < v1.length; i++){
+    console.log(v1[1])
+} */
